6c293c3e2e310be83d86bd16cf799c7d3dfa8787
김은조 <zmffheldk@gmail.com>
2022-12-11 00:06:46 +0900

    success test method

--- src/test/java/com/eunjo/aws/springboot/web/HelloControllerTest.java
+++ src/test/java/com/eunjo/aws/springboot/web/HelloControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,6 +19,7 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Test
     public void returnHello() throws Exception{
         String hello = "hello";
 
