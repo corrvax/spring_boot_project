babd73536d69cf21eb5a45f43aa1dfb33fb19ff3
김은조 <zmffheldk@gmail.com>
2023-01-03 01:13:15 +0900

    CRUD 완성

--- src/main/java/com/eunjo/aws/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/eunjo/aws/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,15 @@
 package com.eunjo.aws.springboot.domain.posts;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     //JpaRepository<Entity 클래스, PK 타입> -> CRUD 자동 생성
     //Entity & Repository 는 함께 위치해야함
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 
 }
--- src/main/java/com/eunjo/aws/springboot/service/posts/PostsService.java
+++ src/main/java/com/eunjo/aws/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.eunjo.aws.springboot.service.posts;
 
 import com.eunjo.aws.springboot.domain.posts.Posts;
 import com.eunjo.aws.springboot.domain.posts.PostsRepository;
+import com.eunjo.aws.springboot.web.dto.PostsListResponseDto;
 import com.eunjo.aws.springboot.web.dto.PostsResponseDto;
 import com.eunjo.aws.springboot.web.dto.PostsSaveRequestDto;
 import com.eunjo.aws.springboot.web.dto.PostsUpdateRequestDto;
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +33,27 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" +id));
         return new PostsResponseDto(entity);
 
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
     }
 }
