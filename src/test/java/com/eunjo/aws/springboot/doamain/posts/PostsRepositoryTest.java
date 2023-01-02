6c91303b94757a0ca259f9ba5f9c3681d9d1b598
김은조 <zmffheldk@gmail.com>
2023-01-02 23:35:37 +0900

    test createdDate and modifiedDate

--- src/test/java/com/eunjo/aws/springboot/doamain/posts/PostsRepositoryTest.java
+++ src/test/java/com/eunjo/aws/springboot/doamain/posts/PostsRepositoryTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.eunjo.aws.springboot.domain.posts.Posts;
 import com.eunjo.aws.springboot.domain.posts.PostsRepository;
+import java.time.LocalDateTime;
 import java.util.List;
+import javafx.geometry.Pos;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -46,6 +48,29 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void RegisterBaseTimeEntity(){//BaseTimeEntity_등록()
+        //given
+        LocalDateTime now = LocalDateTime.of(2022,12,20,0,0,0);
+        postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate =  "+posts.getCreatedDate() +
+            ", modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 
 }
